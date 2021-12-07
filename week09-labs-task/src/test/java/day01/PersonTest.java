6224e7363d5e475ab8a8b183f55100f30a81ae94
Attila <attila.ferenc.80@gmail.com>
2021-12-07 16:12:07 +0100

    day01 PersonTest updated

--- week09-labs-task/src/test/java/day01/PersonTest.java
+++ week09-labs-task/src/test/java/day01/PersonTest.java
@@ -10,9 +10,14 @@ class PersonTest {
 
     @Test
     void initTest() {
+        Ticket ticket = new Ticket("GNR", LocalDateTime.parse("2021-05-14T15:30"), 15_000);
+        FrontOfStageTicket frontOfStageTicket = new FrontOfStageTicket("GNR", LocalDateTime.parse("2021-05-14T15:30"), 18_000, "klsdj");
+
         Person personOne = new Person(new Ticket("GNR", LocalDateTime.parse("2020-05-14T15:30"), 15_000));
         Person personTwo = new Person(new FrontOfStageTicket("GNR", LocalDateTime.parse("2020-05-14T15:30"), 20_000, "kjdfshkjs"));
         assertEquals("GNR", personOne.getTicket().getNameOfBand());
         assertEquals("kjdfshkjs", ((FrontOfStageTicket) personTwo.getTicket()).getExtraCode());
+        assertEquals(ticket.getClass(), personOne.getTicket().getClass());
+        assertEquals(frontOfStageTicket.getClass(), personTwo.getTicket().getClass());
     }
 }
