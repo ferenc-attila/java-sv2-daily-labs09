f43de5c679367dc2e1194e61f0ef6351f42f5790
Attila <attila.ferenc.80@gmail.com>
2021-12-08 19:21:51 +0100

    day03 caesar readability update

--- week09-labs-task/src/main/java/day03/Caesar.java
+++ week09-labs-task/src/main/java/day03/Caesar.java
@@ -2,8 +2,6 @@ package day03;
 
 public class Caesar extends Encryption {
 
-    private static final int FIRST_CHARACTER_VALUE_OF_ABC = 65;
-    private static final int LAST_CHARACTER_VALUE_OF_ABC = 90;
     private int offset;
 
     public Caesar(int offset) {
@@ -37,10 +35,10 @@ public class Caesar extends Encryption {
         int realOffset = offset % 26;
         StringBuilder outputWord = new StringBuilder();
         for (char c : chars) {
-            if (c <= LAST_CHARACTER_VALUE_OF_ABC - realOffset) {
+            if (c <= 'Z' - realOffset) {
                 outputWord.append((char) (c + realOffset));
             } else {
-                outputWord.append((char) (FIRST_CHARACTER_VALUE_OF_ABC - (LAST_CHARACTER_VALUE_OF_ABC + 1 - c) + realOffset));
+                outputWord.append((char) ('A' - ('Z' + 1 - c) + realOffset));
             }
         }
         return outputWord;
